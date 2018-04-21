d618a4380565bcc44d3ca5b61655be1a889f35a2
Colin Woodbury <colingw@gmail.com>
2018-04-21 08:54:06 -0700

    (bench) Local Ops

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -3,21 +3,25 @@
 module Main where
 
 import           Criterion.Main
-import           Data.Massiv.Array as A
+import           Data.List.NonEmpty (NonEmpty(..))
+import           Data.Massiv.Array as A hiding (zipWith)
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as U
 import           Data.Word
 import           Geography.MapAlgebra
 import qualified Numeric.LinearAlgebra as LA
+import           Prelude hiding (zipWith)
 
 ---
 
 main :: IO ()
 main = do
   img <- fileY
+  RGBARaster r' g' b' _ <- fileRGB
   let uv = U.fromList ([1..65536] :: [Int])
       bv = V.fromList ([1..65536] :: [Int])
       cr = greenRed [25, 50, 75, 100, 125, 150, 175, 200, 225, 255]
+      rs = r' :| [g', b']
   defaultMain
     [ bgroup "Raster Creation"
       [ bench "constant 256x256"     $ nf (_array . constantB) 5
@@ -36,54 +40,20 @@ main = do
       [ bench "classify 512x512"  $ nf (_array . strict S . classify invisible cr . lazy) img
       , bench "grayscale 512x512" $ nf (_array . strict S . grayscale . lazy) img
       ]
-      -- [ bgroup "Grayscale"
-        -- [
-      --   bench "encodePng - 256"  $ nf encodePng gray256
-      -- , bench "encodePng - 1024" $ nf encodePng gray1024
-
-      -- , bench "encodeTiff - 256"  $ nf encodeTiff gray256
-      -- , bench "encodeTiff - 1024" $ nf encodeTiff gray1024
-
-      -- , bench "PNG 256 Constant"    $ nf (encodePng . grayscale) (constant 125 :: Raster p 256 256 Word8)
-      -- , bench "PNG 256 fromList"    $ nf (encodePng . grayscale) small
-      -- , bench "PNG 256 fromUnboxed" $ nf (encodePng . grayscale) smallV
-
-      -- , bench "PNG 1024 Constant"    $ nf (encodePng . grayscale) (constant 125 :: Raster p 1024 1024 Word8)
-      -- , bench "PNG 1024 fromList"    $ nf (encodePng . grayscale) big
-      -- , bench "PNG 1024 fromUnboxed" $ nf (encodePng . grayscale) bigV
-
-      -- , bench "TIFF 256 Constant"    $ nf (encodeTiff . grayscale) (constant 125 :: Raster p 256 256 Word8)
-      -- , bench "TIFF 256 fromList"    $ nf (encodeTiff . grayscale) small
-      -- , bench "TIFF 256 fromUnboxed" $ nf (encodeTiff . grayscale) smallV
-
-      -- , bench "TIFF 1024 Constant"    $ nf (encodeTiff . grayscale) (constant 125 :: Raster p 1024 1024 Word8)
-      -- , bench "TIFF 1024 fromList"    $ nf (encodeTiff . grayscale) big
-      -- , bench "TIFF 1024 fromUnboxed" $ nf (encodeTiff . grayscale) bigV
-      --   ]
-      -- , bgroup "RGBA"
-      --   [
-        -- bench "generateImage - 256"  $ nf pixelImg 256
-      -- , bench "generateImage - 1024" $ nf pixelImg 1024
-      -- , bench "encodePng - 256"  $ nf encodePng rgba256
-      -- , bench "encodePng - 1024" $ nf encodePng rgba1024
-
-      -- , bench "encodeTiff - 256"  $ nf encodeTiff rgba256
-      -- , bench "encodeTiff - 1024" $ nf encodeTiff rgba1024
-
-      -- , bench "rgba - 256"  $ nf (encodePng . rgba . classify invisible cmap) small
-      -- , bench "rgba - 256 - 2 ops"  $ nf (\r -> encodePng . rgba . classify invisible cmap $ r + r + r) small
-      -- , bench "rgba - 1024" $ nf (encodePng . rgba . classify invisible cmap) big
-      -- , bench "rgba - 1024 - 2 ops"  $ nf (\r -> encodePng . rgba . classify invisible cmap $ r + r + r) big
-      --   ]
-      -- ]
     -- , bgroup "Massiv Operations"
     --   [ bench "strict S . lazy" $ nf (_array . strict S . lazy) img
     --   , bench "strict U . lazy" $ nf (_array . strict U . lazy) img
     --   , bench "strict P . lazy" $ nf (_array . strict P . lazy) img
     --   ]
     , bgroup "Local Operations"
-      [ -- bench "lmax" $ nf (_array . strict S . lmax img) img
-        -- bench "classify 256" $ nf (classify (PixelRGBA8 0 0 0 0) gray) small
+      [ bench "zipWith"   $ nf (_array . strict S . zipWith (+) img) img
+      , bench "lmax"      $ nf (_array . strict S . lmax img) img
+      , bench "lmin"      $ nf (_array . strict S . lmin img) img
+      , bench "lmean"     $ nf (_array . strict S . lmean @Word8 @Double) rs
+      , bench "lvariety"  $ nf (_array . strict S . lvariety) rs
+      , bench "lmajority" $ nf (_array . strict S . lmajority) rs
+      , bench "lminority" $ nf (_array . strict S . lminority) rs
+      , bench "lvariance" $ nf (fmap (_array . strict S) . lvariance) rs
       ]
     -- , bgroup "HMatrix"
     --   [ bench "linearSolveLS" $ nf (LA.linearSolveLS zing) (LA.col [8,8,8,8,8,8,8,8,8])
@@ -198,4 +168,11 @@ fileY = do
   i <- fromGray "/home/colin/code/haskell/mapalgebra/512x512.tif"
   case i of
     Left err  -> putStrLn err *> pure (constant S Par 8)
+    Right img -> pure img
+
+fileRGB :: IO (RGBARaster p 512 512 Word8)
+fileRGB = do
+  i <- fromRGBA "/home/colin/code/haskell/mapalgebra/512x512.tif"
+  case i of
+    Left err  -> error err
     Right img -> pure img
